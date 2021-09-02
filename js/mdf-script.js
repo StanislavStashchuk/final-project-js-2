53a7c8773fc760621d6f51dc6aae522390809b9f
stanislavstashchuk <stashchuk_stanislav@ukr.net>
2021-09-02 17:04:08 +0300

    initial

--- js/mdf-script.js
+++ js/mdf-script.js
@@ -1,20 +1,20 @@
-const gallery = document.getElementById('mdf-container__gallary');
+const gallery = document.getElementById('mdf-container__gallery');
 const popup = document.getElementById('mdf-popup');
 const selectedImage = document.getElementById('selectedImage');
 
 const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const selectedIndex = null;
 
-imageIndexes.forEach(i => {
+imageIndexes.forEach((i) => {
     const image = document.createElement('img');
-    image.src = `/images/gallaryMdf/mdf-${i}.png`;
+    image.src = `/images/galleryMdf/mdf-${i}.png`;
     image.alt = `Модельний ряд фасадів мдф`;
-    image.classList.add('mdf-gallary__img');
+    image.classList.add('mdf-gallery__img');
 
-    image.addEventListener('click', () => {
-        //popup
+    image.addEventListener('click' , () =>{
+        popup.style.transform = `translateY(0)`;
+        selectedImage.src = `/images/galleryMdf/mdf-${i}.png`;
     });
 
     gallery.appendChild(image);
-
-})
+});
